ae7b5f1c496ac14a64cba36969dc50e44e3e926b
killthebee <51859323+killthebee@users.noreply.github.com>
2020-01-14 16:07:13 +0300

    switches from load_dotenv to environs

--- project/settings.py
+++ project/settings.py
@@ -1,24 +1,25 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+from environs import Env
+env = Env()
+env.read_env()
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.getenv('HOST'),
+        'HOST': env.str('HOST'),
         'PORT': '5434',
         'NAME': 'checkpoint',
         'USER': 'guard',
-        'PASSWORD': os.getenv('PASSWORD'),
+        'PASSWORD': env.str('PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
 ROOT_URLCONF = "project.urls"
 
