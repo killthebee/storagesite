4f7f5cc8873bc3abbea4db56eb325dae8ad6bec0
killthebee <51859323+killthebee@users.noreply.github.com>
2020-01-14 15:52:11 +0300

    hide sensetive data in dotenv

--- project/settings.py
+++ project/settings.py
@@ -1,19 +1,22 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
+        'HOST': os.getenv('HOST'),
         'PORT': '5434',
         'NAME': 'checkpoint',
         'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'PASSWORD': os.getenv('PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
